2157dbe2f2f72ad08f4ba27418f3019ed3e252dc
Maor Rozevich <andory@post.bgu.ac.il>
2018-11-17 03:04:33 +0200

    implemented transitionSystemFromCircuit method

--- src/il/ac/bgu/cs/fvm/impl/FvmFacadeImpl.java
+++ src/il/ac/bgu/cs/fvm/impl/FvmFacadeImpl.java
@@ -14,6 +14,7 @@ import il.ac.bgu.cs.fvm.transitionsystem.AlternatingSequence;
 import il.ac.bgu.cs.fvm.transitionsystem.TransitionSystem;
 import il.ac.bgu.cs.fvm.util.Pair;
 import il.ac.bgu.cs.fvm.verification.VerificationResult;
+import il.ac.bgu.cs.fvm.transitionsystem.Transition;
 import org.svvrl.goal.core.aut.Run;
 
 import java.io.InputStream;
@@ -22,6 +23,9 @@ import java.util.List;
 import java.util.Set;
 import java.util.Map;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Deque;
+import java.util.LinkedList;
 
 /**
  * Implement the methods in this class. You may add additional classes as you
@@ -132,7 +136,6 @@ public class FvmFacadeImpl implements FvmFacade {
 
     private <L1,L2,A> void addInterleavedInitializations(ProgramGraph<Pair<L1, L2>, A> interleavedProgramGraph,
                                                          Set<List<String>> initializationsPG1, Set<List<String>> initializationsPG2){
-
         for(List<String> initializationPG1 : initializationsPG1){
             for(List<String> initializationPG2 : initializationsPG2){
                 List<String> newInitialization = new ArrayList<>(initializationPG1);
@@ -145,7 +148,6 @@ public class FvmFacadeImpl implements FvmFacade {
 
     private <L1,L2,A> void addInterleavedLocations(ProgramGraph<Pair<L1, L2>, A> interleavedProgramGraph,
                                                    Set<L1> initialLocationsPG1, Set<L2> initialLocationsPG2){
-
         for(L1 initialLocationPG1 : initialLocationsPG1){
             for(L2 initialLocationPG2 : initialLocationsPG2){
                 Pair<L1,L2> newLocation = new Pair<>(initialLocationPG1, initialLocationPG2);
@@ -200,14 +202,184 @@ public class FvmFacadeImpl implements FvmFacade {
         return interleavedProgramGraph;
     }
 
-    @Override
-    public TransitionSystem<Pair<Map<String, Boolean>, Map<String, Boolean>>, Map<String, Boolean>, Object> transitionSystemFromCircuit(Circuit c) {
-        throw new UnsupportedOperationException("Not supported yet."); // TODO: Implement transitionSystemFromCircuit
+    private void addLabelToCircuitTransitionSystemIfTrue(
+            TransitionSystem<Pair<Map<String, Boolean>, Map<String, Boolean>>, Map<String, Boolean>, Object> transitionSystem,
+            Pair<Map<String, Boolean>, Map<String, Boolean>> state,
+            Map<String, Boolean> inputs) {
+        for(Map.Entry<String, Boolean> input : inputs.entrySet())
+        {
+            if(input.getValue()) {
+                transitionSystem.addToLabel(state, input.getKey());
+            }
+        }
+    }
+
+    private void addLabelingToTSFromCircuit(TransitionSystem<Pair<Map<String, Boolean>, Map<String, Boolean>>, Map<String, Boolean>, Object> transitionSystem,
+                                            Circuit circuit)
+    {
+        for(Pair<Map<String, Boolean>, Map<String, Boolean>> state: transitionSystem.getStates())
+        {
+            Map<String, Boolean> inputs = state.getFirst();
+            Map<String, Boolean> registers = state.getSecond();
+            Map<String, Boolean> outputs = circuit.computeOutputs(inputs, registers);
+            addLabelToCircuitTransitionSystemIfTrue(transitionSystem, state, inputs);
+            addLabelToCircuitTransitionSystemIfTrue(transitionSystem, state, registers);
+            addLabelToCircuitTransitionSystemIfTrue(transitionSystem, state, outputs);
+
+        }
+    }
+
+    private void circuitTransitionSystemFromInitial(TransitionSystem<Pair<Map<String, Boolean>, Map<String, Boolean>>, Map<String, Boolean>, Object> transitionSystem,
+                  Circuit circuit)
+    {
+        Deque<Pair<Map<String, Boolean>, Map<String, Boolean>>> queue = new LinkedList<>(transitionSystem.getInitialStates());
+        Set<Pair<Map<String, Boolean>, Map<String, Boolean>>> allReadyCheckedStates = new HashSet<>(transitionSystem.getInitialStates());
+        while(!queue.isEmpty())
+        {
+            Pair<Map<String, Boolean>, Map<String, Boolean>> stateToCheck = queue.removeFirst();
+            Map<String, Boolean> newRegisterValues = circuit.updateRegisters(stateToCheck.getFirst(), stateToCheck.getSecond());
+            for(Map<String, Boolean> action : transitionSystem.getActions())
+            {
+                Pair<Map<String, Boolean>, Map<String, Boolean>> newState = new Pair<>(action, newRegisterValues);
+                if(!allReadyCheckedStates.contains(newState))
+                {
+                    allReadyCheckedStates.add(newState);
+                    queue.addLast(newState);
+                    transitionSystem.addState(newState);
+                }
+
+                Transition<Pair<Map<String, Boolean>, Map<String, Boolean>>,Map<String, Boolean>> newTransition = new Transition<>(stateToCheck,
+                        action, newState);
+                transitionSystem.addTransition(newTransition);
+            }
+        }
+    }
+
+    private void addAllAtomicPropositionsFromCircuit(TransitionSystem<Pair<Map<String, Boolean>, Map<String, Boolean>>, Map<String, Boolean>, Object> transitionSystem,
+                  Circuit circuit)
+    {
+        transitionSystem.addAllAtomicPropositions(circuit.getInputPortNames().toArray());
+        transitionSystem.addAllAtomicPropositions(circuit.getOutputPortNames().toArray());
+        transitionSystem.addAllAtomicPropositions(circuit.getRegisterNames().toArray());
+    }
+
+    private void addAllActionsFromCircuitTransition(TransitionSystem<Pair<Map<String, Boolean>, Map<String, Boolean>>, Map<String, Boolean>, Object> transitionSystem)
+    {
+        for(Pair<Map<String, Boolean>, Map<String, Boolean>> initial : transitionSystem.getInitialStates())
+        {
+            transitionSystem.addAction(initial.getFirst());
+        }
+    }
+
+    private String findNextInputNoValue(Map<String, Boolean> inputs, Set<String> inputsPortNames)
+    {
+        for(String input : inputsPortNames)
+        {
+            if(!inputs.containsKey(input))
+                return input;
+        }
+
+        return null;
+    }
+
+    private void createInitialStateFromCircuitAccordingToSize(TransitionSystem<Pair<Map<String, Boolean>, Map<String, Boolean>>, Map<String, Boolean>, Object> transitionSystem,
+                                                     Circuit circuit, Map<String, Boolean> registers, Map<String, Boolean> inputs)
+    {
+        if(inputs.size() == circuit.getInputPortNames().size())
+        {
+            Map<String, Boolean> registersValues = new HashMap<>(registers);
+            Pair<Map<String, Boolean>, Map<String, Boolean>> newState = new Pair<>(inputs, registersValues);
+            transitionSystem.addState(newState);
+            transitionSystem.setInitial(newState, false);
+        }
+        else
+        {
+            String inputWithNoValue = findNextInputNoValue(inputs, circuit.getInputPortNames());
+            Map<String, Boolean> newInputsFalse = new HashMap<>(inputs);
+            Map<String, Boolean> newInputsTrue = new HashMap<>(inputs);
+            newInputsFalse.put(inputWithNoValue, false);
+            createInitialStateFromCircuitAccordingToSize(transitionSystem, circuit, registers, newInputsFalse);
+            newInputsTrue.put(inputWithNoValue, true);
+            createInitialStateFromCircuitAccordingToSize(transitionSystem, circuit, registers, newInputsTrue);
+        }
+    }
+
+    private void createInitialStateFromCircuit(TransitionSystem<Pair<Map<String, Boolean>, Map<String, Boolean>>, Map<String, Boolean>, Object> transitionSystem,
+          Circuit circuit)
+    {
+        Map<String, Boolean> inputs = new HashMap<>();
+        Map<String, Boolean> registers = new HashMap<>();
+        for(String register : circuit.getRegisterNames())
+        {
+            registers.put(register, false);
+        }
+
+        createInitialStateFromCircuitAccordingToSize(transitionSystem, circuit, registers, inputs);
     }
 
     @Override
-    public <L, A> TransitionSystem<Pair<L, Map<String, Object>>, A, String> transitionSystemFromProgramGraph(ProgramGraph<L, A> pg, Set<ActionDef> actionDefs, Set<ConditionDef> conditionDefs) {
-        throw new UnsupportedOperationException("Not supported yet."); // TODO: Implement transitionSystemFromProgramGraph
+    public TransitionSystem<Pair<Map<String, Boolean>, Map<String, Boolean>>, Map<String, Boolean>, Object> transitionSystemFromCircuit(Circuit circuit) {
+        TransitionSystem<Pair<Map<String, Boolean>, Map<String, Boolean>>, Map<String, Boolean>, Object> transitionSystem = createTransitionSystem();
+        createInitialStateFromCircuit(transitionSystem, circuit);
+        addAllActionsFromCircuitTransition(transitionSystem);
+        addAllAtomicPropositionsFromCircuit(transitionSystem, circuit);
+        circuitTransitionSystemFromInitial(transitionSystem, circuit);
+        addLabelingToTSFromCircuit(transitionSystem, circuit);
+        return transitionSystem;
+    }
+
+    private <L, A> void addInitialLocationsTSFromPG(TransitionSystem<Pair<L, Map<String, Object>>, A, String> transitionSystem, ProgramGraph<L, A> pg, Set<ActionDef> actionDefs)
+    {
+        Set<List<String>> variableInitSet = pg.getInitalizations();
+        Set<Map<String, Object>> evals = new HashSet<Map<String,Object>>();
+        Set<L> initialLocations = pg.getInitialLocations();
+
+        for(List<String> variableInit : variableInitSet)
+        {
+            Map<String, Object> variableInitMap = new HashMap<String, Object>();
+            for(String action : variableInit)
+                variableInitMap = ActionDef.effect(actionDefs, variableInitMap, action);
+            evals.add(variableInitMap);
+        }
+
+
+        for(L l : initialLocations)
+        {
+            if(evals.isEmpty())
+            {
+                Pair<L, Map<String, Object>> newState = new Pair<L, Map<String,Object>>(l, new HashMap<String, Object>());
+                transitionSystem.addState(newState);
+                transitionSystem.addInitialState(newState);
+            }
+            else
+            {
+                for(Map<String, Object> eval : evals)
+                {
+                    Pair<L, Map<String, Object>> newState = new Pair<L, Map<String,Object>>(l, new HashMap<String, Object>(eval));
+                    transitionSystem.addState(newState);
+                    transitionSystem.addInitialState(newState);
+                }
+            }
+        }
+    }
+
+    private <L, A> void addAllActionsTSFromPG(TransitionSystem<Pair<L, Map<String, Object>>, A, String> transitionSystem, Set<PGTransition<L, A>> pgTransitions) {
+        for(PGTransition<L, A> pgTransition : pgTransitions) {
+            transitionSystem.addAction(pgTransition.getAction());
+        }
+    }
+
+    @Override
+    public <L, A> TransitionSystem<Pair<L, Map<String, Object>>, A, String> transitionSystemFromProgramGraph(ProgramGraph<L, A> programGraph,
+                 Set<ActionDef> actionDefinitions, Set<ConditionDef> conditionDefinitions) {
+        TransitionSystem<Pair<L, Map<String, Object>>, A, String> transitionSystem = createTransitionSystem();
+        addAllActionsTransitionSystemFromPG(transitionSystem, programGraph.getTransitions());
+        addInitialLocationsTSFromPG(transitionSystem, programGraph, actionDefinitions);
+        transitionSystemFromProgramGraphFromInitialStates(transitionSystem, programGraph, actionDefinitions, conditionDefinitions);
+        addAtomicProposition(transitionSystem);
+        addLabelsToTSFromPG(transitionSystem);
+        removeActionsNotReachable(transitionSystem);
+        return transitionSystem;
     }
 
     @Override
